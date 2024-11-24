4927f3b94f7473267344d5078877aba7122025d8
Sergey Temkin <sergeytemkin@gmail.com>
2024-11-24 20:19:30 +0200

    Added Fronend

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,18 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'products',
 ]
 
+# Impotent to paste new "MIDDLEWARE" in the right line(Let ChatGPT show you where) 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'myshop.urls'
 
